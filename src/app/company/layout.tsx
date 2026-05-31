a222cf6923d01bc54370144bf27d13e1738fbe4d
ziad badry <badry8977@gmail.com>
2026-05-31 18:13:07 +0300

    ui editis

--- src/app/company/layout.tsx
+++ src/app/company/layout.tsx
@@ -1,12 +1,17 @@
+"use client"
 import type { ReactNode } from "react";
 import Footer from "@/components/shared/footer";
+import { usePathname } from "next/navigation";
 
 export default function CompanyLayout({ children }: { children: ReactNode }) {
+  const path=usePathname()
+  const isForbidden= path==='/company/profile' || path==='/company/settings' || path==='/company/verification'
+  console.log(path,'dsdsds')
   return (
     <div className="min-h-screen flex flex-col text-white">
       <main className="flex-1">{children}</main>
 
-      <Footer />
+      {!isForbidden && <Footer />}
     </div>
   );
 }
