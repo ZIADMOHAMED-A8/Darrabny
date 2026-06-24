e32681dece5a161c2eb122e6b0513aee1bfc599d
Omda112 <emadehab467@gmail.com>
2026-06-24 07:18:54 +0300

    gmail - github

--- src/app/(auth)/login/_components/login-form.tsx
+++ src/app/(auth)/login/_components/login-form.tsx
@@ -216,7 +216,7 @@ export default function LoginFormCard({
               <Separator className="flex-1 bg-black/10" />
             </div>
 
-            <div className="grid grid-cols-2 gap-4">
+            {/* <div className="grid grid-cols-2 gap-4">
               <Button
                 type="button"
                 variant="secondary"
@@ -234,7 +234,7 @@ export default function LoginFormCard({
               >
                 <Github className="mr-2 h-5 w-5" /> GitHub
               </Button>
-            </div>
+            </div> */}
           </form>
         </Form>
       </CardContent>
